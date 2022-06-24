b6e4bfd364c8e9f529b6badce920dd6d9fb9507a
Sheroziy <testsmacbookairm1@testss-macbook-air.home>
2022-06-25 00:10:37 +0200

    fixed workflow

--- .github/scripts/rename_build.py
+++ .github/scripts/rename_build.py
@@ -6,6 +6,6 @@ apk = 'build/app/outputs/flutter-apk/app-release.apk'
 
 date_string = datetime.now().strftime('%Y%m%H%M')
 
-new_apk_name = 'build/app/outputs/flutter-apk/app-release-'+ date_string +'.apk'
+new_apk_name = 'build/app/outputs/flutter-apk/waiter-app-release-'+ date_string +'.apk'
 
 os.rename(apk, new_apk_name)
