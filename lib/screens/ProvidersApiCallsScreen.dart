2949fb7df5f93587f4919336459772f9779bc5ee
Andi7945 <haering.andi@gmail.com>
2022-10-08 18:22:04 +0200

    changed loading order of APICalls

--- lib/screens/ProvidersApiCallsScreen.dart
+++ lib/screens/ProvidersApiCallsScreen.dart
@@ -40,13 +40,13 @@ class _ProvidersApiCallsState extends State<ProvidersApiCalls> {
   Widget build(BuildContext context) {
     return _isLoading == true
         ? Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Center(
-      child: Lottie.asset('assets/lottie/cateringfork-knife.json'),
-    ),
-          ),
-        )
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Lottie.asset('assets/lottie/cateringfork-knife.json'),
+        ),
+      ),
+    )
         : const HomePage();
   }
 
@@ -54,35 +54,40 @@ class _ProvidersApiCallsState extends State<ProvidersApiCalls> {
     // to load all the data that the user needs
     final tablesData = Provider.of<Tables>(context, listen: false);
     //final token = Provider.of<Authy>(context, listen: false).token;
-    {
+        {
       //tablesData.addTabl(token: token).then((_) async {
-        final tokenProvider = Provider.of<Authy>(context, listen: false);
-        final token = tokenProvider.token;
-        await Provider.of<Categorys>(context, listen: false)
-            .addCategory(context: context, token: token);
-        _isInit = false;
-        await Provider.of<Ingredients>(context, listen: false)
-            .addIngredients(token: token, context: context);
-        await Provider.of<Products>(context, listen: false)
-            .addProducts(token: token, context: context);
-        //await Provider.of<DipsProvider>(context, listen: false).addDips(token: token, context: context); Remove Dips 27.07 Andi
-        await Provider.of<WorkersProvider>(context, listen: false)
-            .addWorkers(token: token, context: context);
-        await Provider.of<NotificationProvider>(context, listen: false).addNotificationType(token: token, context: context);
+      final tokenProvider = Provider.of<Authy>(context, listen: false);
+      final token = tokenProvider.token;
 
-        await tablesData.connectALlTablesSocket(context: context, token: token);
-         await tablesData.listenToAllTabelsSocket(
-             context: context, token: token);
-        for (var i = 0; i < tablesData.items.length; i++) {
-          var table = tablesData.items[i];
-          await tablesData.connectSocket(
-              id: table.id, context: context, token: token);
-           await tablesData.listenSocket(
-               id: table.id, context: context, token: token);
-        }
-        setState(() {
-          _isLoading = false;
-        });
+      await tablesData.connectALlTablesSocket(context: context, token: token);
+      await tablesData.listenToAllTabelsSocket(
+          context: context, token: token);
+
+
+      await Provider.of<Categorys>(context, listen: false)
+          .addCategory(context: context, token: token);
+      _isInit = false;
+      await Provider.of<Ingredients>(context, listen: false)
+          .addIngredients(token: token, context: context);
+      await Provider.of<Products>(context, listen: false)
+          .addProducts(token: token, context: context);
+      //await Provider.of<DipsProvider>(context, listen: false).addDips(token: token, context: context); Remove Dips 27.07 Andi
+      await Provider.of<WorkersProvider>(context, listen: false)
+          .addWorkers(token: token, context: context);
+      await Provider.of<NotificationProvider>(context, listen: false).addNotificationType(token: token, context: context);
+
+
+      for (var i = 0; i < tablesData.items.length; i++) {
+        var table = tablesData.items[i];
+        await tablesData.connectSocket(
+            id: table.id, context: context, token: token);
+        await tablesData.listenSocket(
+            id: table.id, context: context, token: token);
+
+      }
+      setState(() {
+        _isLoading = false;
+      });
       //});
     }
   }
