14796173e52eaf6fbe9ed8942f5cd5d5f6d76085
SheroziyCODE <sheroziy.saidkhodjaev@code.berlin>
2022-11-08 23:30:03 +0100

    last one

--- .github/scripts/rename_build.py
+++ .github/scripts/rename_build.py
@@ -3,12 +3,10 @@ from pathlib import Path
 import sys
 import os
 
-apk = 'build/app/outputs/flutter-apk/app-release.apk'
-
 build_name = sys.argv[1]
 
 build_number = sys.argv[2]
 
-new_apk_name = 'build/app/outputs/flutter-apk/inserve-app-release-'+ build_name + "("+build_number+")" +'.apk'
+new_apk_name = "build/app/outputs/flutter-apk/inserve-app-release-"+ build_name + "-("+build_number+")" +".apk"
 
 os.rename(apk, new_apk_name)
